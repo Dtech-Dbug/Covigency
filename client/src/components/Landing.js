cd77e80ae68ba66b81e37c6468a8a5f94c4497d9
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-01 00:21:26 +0530

    docs 🛸: created Landing.js inside components folder

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import "../App.css";
+import logo from "../images/covigency.svg";
+
+function LandingPage() {
+	return (
+		<div className="App">
+			<img src={logo} alt="Opps" />
+		</div>
+	);
+}
+
+export default LandingPage;
