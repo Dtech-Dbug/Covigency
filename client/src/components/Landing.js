fa1825966d68478b39efd5b227eaa1a32ea34476
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-01 02:01:40 +0530

    update 🚀: LandingPage.js : button, footerText

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,10 +3,34 @@ import logo from "../images/covigency.svg";
 
 function LandingPage() {
 	return (
-		<div className="App">
-			<img src={logo} alt="Opps" />
+		<div className="LandingPage">
+			<header>
+				<img src={logo} alt="Opps" />
+				<figcaption>
+					<span className="subHeading-by">By</span>
+					<span className="subHeading">NBSC</span>
+					<span className="subHeading">FOUNDATION</span>
+				</figcaption>
+			</header>
+
+			<footer>
+				<div className="button-wrap">
+					<button>
+						<p>Get Started</p>
+					</button>
+				</div>
+				<p className="footer-footer" style={{ color: "white" }}>
+					By getting started you accept our{" "}
+					<span>
+						<b>Terms & Conditions</b>
+					</span>
+				</p>
+			</footer>
 		</div>
 	);
 }
 
 export default LandingPage;
+
+//<details style={{ color: "white" }}>hello</details>
+//<button>Save</button>
