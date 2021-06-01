bdd2e7a93badc4d4e73e259c3478d0b9fe815245
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-02 02:34:30 +0530

    updated🚀:  states.js

--- client/src/components/states.js
+++ client/src/components/states.js
@@ -1,10 +1,24 @@
 import React from "react";
 import BrowseIcon from "../images/Browse icon.svg";
 import EmergencyIcon from "../images/emergencyicon.svg";
+import { Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+
 const States = () => {
 	return (
-		<div className="StatesContainer" style={{ background: "white" }}>
-			<footer style={{ background: "crimson" }}>
+		<div className="StatesContainer">
+			<div className="headerText">
+				<h1>HOME</h1>
+
+				<div className="state-search-input">
+					<i class="fa fa-search"></i>
+					<input type="text" placeholder="Search Your State here"></input>
+				</div>
+			</div>
+
+			<div className="states-content-container"></div>
+
+			<footer>
 				<ul>
 					<li>
 						<img src={EmergencyIcon} alt="Unavailable" />
