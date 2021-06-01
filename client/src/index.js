2a440c78cf0ed547b91f46d2d74afbcaae3e4516
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-02 01:03:37 +0530

    update 🚀:index.js => imported antd css files

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+//importing the css for antd , else the adnt designs look terribly messy
+import "antd/dist/antd.css";
 
 ReactDOM.render(
 	<React.StrictMode>
