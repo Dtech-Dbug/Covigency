fb1c2c01636b800c886fa8aec7fef6d0a59bff54
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-03 00:50:15 +0530

    update 🚀: states.js : wrapped the contents inside a div , except for the footer

--- client/src/components/states.js
+++ client/src/components/states.js
@@ -8,27 +8,31 @@ import { SearchOutlined } from "@ant-design/icons";
 const States = () => {
 	return (
 		<div className="StatesContainer">
-			<div className="headerText">
-				<h1>HOME</h1>
+			<div className="statesContainer-Content">
+				<div className="headerText">
+					<h1>HOME</h1>
 
-				<div className="state-search-input">
-					<i class="fa fa-search"></i>
-					<input type="text" placeholder="Search Your State here"></input>
+					<div className="state-search-input">
+						<i class="fa fa-search"></i>
+						<input type="text" placeholder="Search Your State here"></input>
+					</div>
 				</div>
-			</div>
 
-			<div className="states-content-container">
-				<h2
-					style={{
-						textAlign: "left",
-						paddingLeft: "1rem",
-						marginBottom: "0.2rem",
-						color: "#868686",
-					}}
-				>
-					Select State
-				</h2>
-				<StatesGrid />
+				<div className="states-content-container">
+					<h2
+						style={{
+							textAlign: "left",
+							paddingLeft: "1rem",
+							marginBottom: "0.2rem",
+							color: "white",
+							fontSize: "1.2rem",
+							fontFamily: "var(--text-medium)",
+						}}
+					>
+						Select State
+					</h2>
+					<StatesGrid />
+				</div>
 			</div>
 
 			<footer>
