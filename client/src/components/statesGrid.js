08fc7cefa08ed00b4846480c801313a5a1695702
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-02 15:19:37 +0530

    docs(): 🚀: created statesGrid.js component for grid view of all states

---
+++ client/src/components/statesGrid.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const StatesGrid = () => {
+	return (
+		<div className="states-parent-grid-container">
+			<div className="states-child-grid-container top-child">
+				<div className="child child-1">AN</div>
+				<div className="state-name">Arunachal Pradesh</div>
+			</div>
+
+			<div className="states-child-grid-container top-child">
+				<div className="child child-2">AP</div>
+			</div>
+
+			<div className="states-child-grid-container">
+				<div className="child child-3">AR</div>
+			</div>
+
+			<div className="states-child-grid-container">
+				<div className="child child-4">AP</div>
+			</div>
+
+			<div className="states-child-grid-container">
+				<div className="child child-5">AS</div>
+			</div>
+
+			<div className="states-child-grid-container">
+				<div className="child child-6">BR</div>
+			</div>
+
+			<div className="states-child-grid-container">
+				<div className="child child-7">CH</div>
+			</div>
+
+			<div className="states-child-grid-container">
+				<div className="child child-8">CG</div>
+			</div>
+
+			<div className="states-child-grid-container">
+				<div className="child child-9">DN</div>
+			</div>
+		</div>
+	);
+};
+
+export default StatesGrid;
