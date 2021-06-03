e30c5dcfc5e72cd0b929f2bb7611aac6b4823ac7
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-03 21:28:43 +0530

    dpcs () ✍ : create Cities.js components

---
+++ client/src/components/UserComponents/Cities.js
@@ -0,0 +1,50 @@
+import React from "react";
+import StatesGrid from "../UserComponents/statesGrid";
+import StickyFooter from "../reusableComponents/stickyFooter";
+import CityLists from "./cityLists";
+
+const Cities = () => {
+	return (
+		<div className="Main-Container">
+			<div className="Main-Container-Content">
+				<div className="headerText">
+					<div className="header-and-logo">
+						<div className="header">
+							<h1>HOME</h1>
+						</div>
+
+						<div className="logo">
+							<div className="stateAvatar">
+								<div className="state-avatar-name">AP</div>
+							</div>
+						</div>
+					</div>
+
+					<div className="state-search-input">
+						<i class="fa fa-search"></i>
+						<input type="text" placeholder="Search Your CIty here"></input>
+					</div>
+				</div>
+
+				<div className="states-content-container">
+					<h2
+						style={{
+							textAlign: "left",
+							paddingLeft: "1rem",
+							marginBottom: "0.2rem",
+							color: "white",
+							fontSize: "1.2rem",
+							fontFamily: "var(--text-medium)",
+						}}
+					>
+						Select City
+					</h2>
+					<CityLists />
+				</div>
+			</div>
+			<StickyFooter />
+		</div>
+	);
+};
+
+export default Cities;
