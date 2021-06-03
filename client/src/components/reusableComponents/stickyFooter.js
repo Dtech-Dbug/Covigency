023208bf7e35823f9f2ef19d73bd5be16b0ae19a
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-03 19:37:26 +0530

    create 🚀: moved StickyFooter to its own component , reusable

---
+++ client/src/components/reusableComponents/stickyFooter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import EmergencyIcon from "../../images/emergencyicon.svg";
+import BrowseIcon from "../../images/Browse icon.svg";
+
+const StickyFooter = () => {
+	return (
+		<footer>
+			<ul>
+				<li>
+					<img src={EmergencyIcon} alt="Unavailable" />
+				</li>
+				<li>
+					<img src={BrowseIcon} alt="Unavailable" />
+				</li>
+			</ul>
+		</footer>
+	);
+};
+
+export default StickyFooter;
