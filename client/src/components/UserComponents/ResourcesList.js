432a987f1e4dc2e004ae38d567e32bdf04847deb
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-05 11:03:15 +0530

    docs () 👨‍💻:  ResourcesList.js component created
    Resources List based on state and cities

---
+++ client/src/components/UserComponents/ResourcesList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../reusableComponents/header";
+
+const ResourcesLIst = () => {
+	return (
+		<div className="fluid-container">
+			<div className="row">
+				<div className="col-md-12  m-1">
+					<Header />
+				</div>
+			</div>
+		</div>
+	);
+};
+export default ResourcesLIst;
