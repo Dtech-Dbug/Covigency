a425380c49690b1ee8a0068da96d1c732efec4f3
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-08 00:36:21 +0530

    docs ()🚀: create HeaderHomeWithCSS.js
    - Reusable header component with CSS

---
+++ client/src/components/reusableComponents/HomeHeaderWithCSS.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomeHeaderWithCSS = () => {
+	return (
+		<div className="Main-Container-Content">
+			<div className="headerText">
+				<h1>HOME</h1>
+			</div>
+		</div>
+	);
+};
+
+export default HomeHeaderWithCSS;
