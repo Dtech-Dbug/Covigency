83a537ccd9c735d2fbba0ce93774d3007b0ba054
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-08 11:58:31 +0530

    update 🚀 HomeHeaderWithCSS
    -included the search box in the header component

--- client/src/components/reusableComponents/HomeHeaderWithCSS.js
+++ client/src/components/reusableComponents/HomeHeaderWithCSS.js
@@ -5,6 +5,11 @@ const HomeHeaderWithCSS = () => {
 		<div className="Main-Container-Content">
 			<div className="headerText">
 				<h1>HOME</h1>
+
+				<div className="state-search-input">
+					<i class="fa fa-search"></i>
+					<input type="text" placeholder="Search Your State here"></input>
+				</div>
 			</div>
 		</div>
 	);
