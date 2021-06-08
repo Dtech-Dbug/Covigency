4f7d0f52c47543fac1b3b6131f0fbc7323d41a5a
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-08 12:01:52 +0530

    update 🚀: App.cs
    - Imported newly created component: Categories.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import States from "./components/UserComponents/states";
 import Cities from "./components/UserComponents/Cities";
 import { path, Route } from "react-router-dom";
 import Resources from "./components/UserComponents/Resources";
+import Categories from "./components/UserComponents/Categories";
 
 function App() {
 	return (
 		<div className="App">
 			<Route path="/" exact component={LandingPage} />
+			<Route path="/categories" exact component={Categories} />
 			<Route path="/states" exact component={States} />
 			<Route path="/cities" exact component={Cities} />
 			<Route path="/resources" exact component={Resources} />
