911ac405125d0af6640ec4f4f70ab3bf882d8bea
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-09 12:50:17 +0530

    docs () 🚀: created Vaccine Folder to have files related to vaccine card
    - Promotion Card
    - Vaccine.css : exlcusive stylesheets for vaccine component scope

---
+++ client/src/components/UserComponents/Vaccine/PromotionCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../Vaccine/vaccine.css";
+
+const PromotionCard = () => {
+	return (
+		<div className="promotion-card">
+			<div className="promoted-org">
+				<h4> Join </h4>
+				<h1>NBSC Foundation</h1>
+			</div>
+
+			<div className="promoted-org-intro">
+				<p>
+					In a country that is home to the world's second-largest population ,
+					when one of it's largest enterprises providentially happens to be a
+					philanthropic organisation, It raises hope for a promising and
+					sustainable future.
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default PromotionCard;
