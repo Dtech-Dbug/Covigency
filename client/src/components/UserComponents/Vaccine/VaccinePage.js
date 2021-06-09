771d07f6d48617fa28992c4817e7cd084c290544
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-09 15:53:13 +0530

    docs () 🚀:  created VaccinePage.js Component
    - Imported PromotionCard Component
    - Imported Vaccine-Info component

---
+++ client/src/components/UserComponents/Vaccine/VaccinePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PromotionCard from "./PromotionCard";
+import VaccineInfo from "./Vaccine-Info";
+
+export const VaccinePage = () => {
+	return (
+		<div id="Vaccine-Container">
+			<PromotionCard />
+			<VaccineInfo />
+		</div>
+	);
+};
