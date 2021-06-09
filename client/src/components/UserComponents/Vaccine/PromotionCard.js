efd5d0c3e97974ef8306f95b1fe2db77888be762
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-09 14:30:00 +0530

    docs () ✍: created promotionCard.js
    - A reusable and easily maintainable component
    -Promotion of other NGO's
    -Arrow Button : Yet to be linked with promoted NGO'S website

--- client/src/components/UserComponents/Vaccine/PromotionCard.js
+++ client/src/components/UserComponents/Vaccine/PromotionCard.js
@@ -1,22 +1,27 @@
 import React from "react";
 import "../Vaccine/vaccine.css";
+import vaccineBG from "../../../BackgroundImages/Rectangle 11.svg";
+import Arrow from "../../../icons/Arrow.svg";
 
 const PromotionCard = () => {
 	return (
 		<div className="promotion-card">
 			<div className="promoted-org">
-				<h4> Join </h4>
+				<h4>Join</h4>
 				<h1>NBSC Foundation</h1>
 			</div>
 
-			<div className="promoted-org-intro">
+			<div className="promotion-intro">
 				<p>
-					In a country that is home to the world's second-largest population ,
-					when one of it's largest enterprises providentially happens to be a
-					philanthropic organisation, It raises hope for a promising and
-					sustainable future.
+					In a country that is home to the world’s second-largest population,
+					when one of its largest enterprises providentially happens to be a
+					philanthropic organisation, it raises hopes for a promising and
+					sustainable future
 				</p>
 			</div>
+
+			<div className="promotion-footer-middle">Promoted ad*</div>
+			<img src={Arrow} alt="oops" />
 		</div>
 	);
 };
