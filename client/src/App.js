aef319f2532c298e2edc388127b2fa56ff06d541
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-10 02:22:04 +0530

    Update 🚀: App.js : imported categories Component

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,21 @@ import States from "./components/UserComponents/states";
 import Cities from "./components/UserComponents/Cities";
 import { path, Route } from "react-router-dom";
 import Resources from "./components/UserComponents/Resources";
-import Categories from "./components/UserComponents/Categories";
+import PromotionCard from "./components/UserComponents/Vaccine/PromotionCard";
+import VaccineInfo from "./components/UserComponents/Vaccine/Vaccine-Info";
+import { VaccinePage } from "./components/UserComponents/Vaccine/VaccinePage";
+import CategoryGrid from "./components/UserComponents/Categories/CategoryGrid";
+import SelectCategories from "./components/UserComponents/Categories/SelectCategories";
 
 function App() {
 	return (
 		<div className="App">
 			<Route path="/" exact component={LandingPage} />
-			<Route path="/categories" exact component={Categories} />
 			<Route path="/states" exact component={States} />
 			<Route path="/cities" exact component={Cities} />
 			<Route path="/resources" exact component={Resources} />
+			<Route path="/vaccine" exact component={VaccinePage} />
+			<Route path="/categories" exact component={SelectCategories} />
 		</div>
 	);
 }
