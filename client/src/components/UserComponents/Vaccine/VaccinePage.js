4c6a62fd4ad6fe86d81eb760f0a91962feb6fc78
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-09 19:26:37 +0530

    Docs 🚀: created The VaccinePage.js component and working on stylings
    -imported vaccine-info component
    -imported promotion-card component
    - Stylings

--- client/src/components/UserComponents/Vaccine/VaccinePage.js
+++ client/src/components/UserComponents/Vaccine/VaccinePage.js
@@ -1,12 +1,17 @@
 import React from "react";
+import StickyFooter from "../../reusableComponents/stickyFooter";
+import "../Vaccine/vaccine.css";
 import PromotionCard from "./PromotionCard";
 import VaccineInfo from "./Vaccine-Info";
 
 export const VaccinePage = () => {
 	return (
 		<div id="Vaccine-Container">
-			<PromotionCard />
-			<VaccineInfo />
+			<div className="vaccine-content-container">
+				<PromotionCard />
+				<VaccineInfo />
+			</div>
+			<StickyFooter />
 		</div>
 	);
 };
