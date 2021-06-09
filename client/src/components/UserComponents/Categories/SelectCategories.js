f12ace1a38d5fe421b428930d41308755d4aa001
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-10 02:31:32 +0530

    Update 🚀docs && Stylings : category Component

--- client/src/components/UserComponents/Categories/SelectCategories.js
+++ client/src/components/UserComponents/Categories/SelectCategories.js
@@ -1,11 +1,14 @@
 import React from "react";
 import HomeHeaderBootstrap from "../../reusableComponents/HomeHeaderBootstrap";
+import HomeHeaderWithCSS from "../../reusableComponents/HomeHeaderWithCSS";
+import StickyFooter from "../../reusableComponents/stickyFooter";
 import CategoryGrid from "./CategoryGrid";
 
 const SelectCategories = () => {
 	return (
 		<div id="Select-Categories-Container-Parent">
 			<div className="Select-Categories-Content-Container">
+				<HomeHeaderBootstrap />
 				<CategoryGrid />
 			</div>
 		</div>
