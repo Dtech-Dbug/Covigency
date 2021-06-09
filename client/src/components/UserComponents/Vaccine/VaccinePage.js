d3ccb6f046fdc28636ffb4112735eeba61c0f94b
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-09 20:07:53 +0530

    update 🚀: docs and stylings updated Vaccine.js /vaccine.css

--- client/src/components/UserComponents/Vaccine/VaccinePage.js
+++ client/src/components/UserComponents/Vaccine/VaccinePage.js
@@ -3,11 +3,13 @@ import StickyFooter from "../../reusableComponents/stickyFooter";
 import "../Vaccine/vaccine.css";
 import PromotionCard from "./PromotionCard";
 import VaccineInfo from "./Vaccine-Info";
+import Arrow from "../../../icons/Arrow icon.svg";
 
 export const VaccinePage = () => {
 	return (
 		<div id="Vaccine-Container">
 			<div className="vaccine-content-container">
+				<h2> {"<"} Home </h2>
 				<PromotionCard />
 				<VaccineInfo />
 			</div>
