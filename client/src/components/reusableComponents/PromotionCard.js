563545836e2597c601474e5a7636e05171d7e103
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-15 22:55:20 +0530

    Update🚀 ; PromotionCard docs && stylings
    - Dynamic links to promotioncard
    - Changed stylings
    -- a : no text-decoration
    -- p : color : black

--- client/src/components/reusableComponents/PromotionCard.js
+++ client/src/components/reusableComponents/PromotionCard.js
@@ -1,23 +1,25 @@
 import React from "react";
 import "../stylesheets/PromotionCard.css";
 
-const PromotionCard = ({ header, img, paragraph, extension }) => {
+const PromotionCard = ({ header, img, paragraph, extension, LinkToOrg }) => {
 	return (
-		<div className="promotion-card">
-			<div className="promoted-org">
-				<h4>Join</h4>
-				<h1>
-					{header} <span>{extension}</span>{" "}
-				</h1>
-			</div>
+		<a href={LinkToOrg} target="_blank">
+			<div className="promotion-card">
+				<div className="promoted-org">
+					<h4>Join</h4>
+					<h1>
+						{header} <span>{extension}</span>{" "}
+					</h1>
+				</div>
 
-			<div className="promotion-intro">
-				<p>{paragraph}</p>
-			</div>
+				<div className="promotion-intro">
+					<p>{paragraph}</p>
+				</div>
 
-			<div className="promotion-footer-middle">Promoted ad*</div>
-			<img src={img} alt="oops" />
-		</div>
+				<div className="promotion-footer-middle">Promoted ad*</div>
+				<img src={img} alt="oops" />
+			</div>
+		</a>
 	);
 };
 
