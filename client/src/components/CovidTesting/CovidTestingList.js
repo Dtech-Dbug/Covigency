dc6e7dc0cf6db26a4bfb86ab34cf38f38116a3fd
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-24 20:21:59 +0530

    update Covidtesting.js
    - passed name as params and pushed to next route using the name in the params

--- client/src/components/CovidTesting/CovidTestingList.js
+++ client/src/components/CovidTesting/CovidTestingList.js
@@ -1,42 +1,57 @@
 import React, { Fragment } from "react";
 
+//import UseHistory Hook for pushing routes
+import { useHistory } from "react-router-dom";
+
 import "./CovidTesting.css";
 
 const CovidTestingList = ({ HospitalList }) => {
-  const checkAvailability = (availability) => {
-    if (availability === "full") {
-      return "availabilityStatus-full";
-    } else if (availability === "moderate") {
-      return "availabilityStatus-moderate";
-    } else {
-      return "availabilityStatus-free";
-    }
-  };
-  return (
-    <Fragment>
-      {" "}
-      <h3 className="text-light">Covid-19 Testing</h3>
-      <div className="grid grid-1x2 resource-block">
-        {HospitalList.map(({ id, name, address, availabilityStatus }) => {
-          return (
-            <div key={id} className="covid-testing card card--dark">
-              <span
-                className={`availabilityStatus ${checkAvailability(
-                  availabilityStatus
-                )}`}
-                src={availabilityStatus}
-              />
+	const history = useHistory();
 
-              <div className="card__header">
-                <h3 className="card__title">{name}</h3>
-                <p className="card__description">{address}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </Fragment>
-  );
+	const checkAvailability = (availability) => {
+		if (availability === "full") {
+			return "availabilityStatus-full";
+		} else if (availability === "moderate") {
+			return "availabilityStatus-moderate";
+		} else {
+			return "availabilityStatus-free";
+		}
+	};
+
+	function routeToHospitalInfo(name) {
+		console.log(name); //console.log name to be sure
+
+		history.push(`/hospital/${name}`);
+	}
+	return (
+		<Fragment>
+			{" "}
+			<h3 className="text-light">Covid-19 Testing</h3>
+			<div className="grid grid-1x2 resource-block">
+				{HospitalList.map(({ id, name, address, availabilityStatus }) => {
+					return (
+						<div
+							key={id}
+							className="covid-testing card card--dark"
+							onClick={() => routeToHospitalInfo(name)}
+						>
+							<span
+								className={`availabilityStatus ${checkAvailability(
+									availabilityStatus
+								)}`}
+								src={availabilityStatus}
+							/>
+
+							<div className="card__header">
+								<h3 className="card__title">{name}</h3>
+								<p className="card__description">{address}</p>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</Fragment>
+	);
 };
 
 export default CovidTestingList;
