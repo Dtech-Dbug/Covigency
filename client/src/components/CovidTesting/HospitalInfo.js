b39af4acee5e9667bb38260426c9c0fd8aa88d6c
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-24 20:59:21 +0530

    update 🚀: HospitalInfo.js
    - caught the id from match.params.id
    
    - searched the Hospitaldata with the index no.
    
    - set state with retrived data
    
    - show the data on UI

--- client/src/components/CovidTesting/HospitalInfo.js
+++ client/src/components/CovidTesting/HospitalInfo.js
@@ -6,13 +6,29 @@ const HospitalInfo = ({ match }) => {
 
 	useEffect(() => {
 		setHospital(HospitalList[match.params.name]);
-		console.log("hospitalInfo", hospital);
+		console.log("hospitalInfo", hospital); //to check if we are getting the data correctly
 		console.log(match.params.name);
 	});
 	return (
 		<div>
 			<h3 style={{ color: "white" }}>hello</h3>
 			<h3 style={{ color: "white" }}>{match.params.name}</h3>
+			{hospital && (
+				<>
+					{" "}
+					<h3 style={{ color: "white" }}>{hospital.name}</h3>
+					<h3 style={{ color: "white" }}>{hospital.contactNo}</h3>{" "}
+				</>
+			)}
+
+			{/* {hospital && (
+				<>
+					<h3 style={{ color: "white" }}>{hospital.name}</h3>
+					<h3 style={{ color: "white" }}>{hospital.address}</h3>
+					<h3 style={{ color: "white" }}>{hospital.contactNo}</h3>
+					<h3 style={{ color: "white" }}>{hospital.timing}</h3>
+				</>
+			)} */}
 		</div>
 	);
 };
