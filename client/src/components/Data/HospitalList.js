9a5c5b4f9fd5907a3c85ff3982cb30b0bc93cc88
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-24 20:58:03 +0530

    update 🚀: HospitalList.js
    - changed the id, zero based index
    
    - for better communication between components

--- client/src/components/Data/HospitalList.js
+++ client/src/components/Data/HospitalList.js
@@ -1,57 +1,57 @@
 const HospitalList = [
-  {
-    id: 1,
-    name: "Aashlok Hospital",
-    address:
-      "25 AB, Community Centre, Safdarjung Enclave Safdarjung, New Delhi - 110029",
-    contactNo: 8759856466,
-    availabilityStatus: "free",
-    timing: "8:00AM to 8:00PM",
-  },
-  {
-    id: 2,
-    name: "Saroj Hospital",
-    address:
-      "Near Madhuban Chowk, Sector 14, Rohini Rohini, New Delhi - 110085",
-    contactNo: 8759856466,
-    availabilityStatus: "free",
-    timing: "8:00AM to 8:00PM",
-  },
-  {
-    id: 3,
-    name: "Batra Hospital",
-    address:
-      "1, Mehrauli Badarpur Road, Tughlakabad Institutional Area Tughlakabad, New Delhi - 110062",
-    contactNo: 8759856466,
-    availabilityStatus: "full",
-    timing: "8:00AM to 8:00PM",
-  },
-  {
-    id: 4,
-    name: "Orchid Hospital",
-    address:
-      "C-3/91-92, Near Fly Over, Dabri More Janakpuri, New Delhi - 110058",
-    contactNo: 8759856466,
-    availabilityStatus: "free",
-    timing: "8:00AM to 8:00PM",
-  },
-  {
-    id: 5,
-    name: "Medanta Hospital",
-    address: "E – 18 Defence Colony , New Delhi - 110024",
-    contactNo: 8759856466,
-    availabilityStatus: "moderate",
-    timing: "8:00AM to 8:00PM",
-  },
-  {
-    id: 6,
-    name: "Vardaan Hospital",
-    address:
-      "B-100, Near Malviya Nagar Market Malviya Nagar, New Delhi - 110017",
-    contactNo: 8759856466,
-    availabilityStatus: "full",
-    timing: "8:00AM to 8:00PM",
-  },
+	{
+		id: 0,
+		name: "Aashlok Hospital",
+		address:
+			"25 AB, Community Centre, Safdarjung Enclave Safdarjung, New Delhi - 110029",
+		contactNo: 8759856466,
+		availabilityStatus: "free",
+		timing: "8:00AM to 8:00PM",
+	},
+	{
+		id: 1,
+		name: "Saroj Hospital",
+		address:
+			"Near Madhuban Chowk, Sector 14, Rohini Rohini, New Delhi - 110085",
+		contactNo: 8759856466,
+		availabilityStatus: "free",
+		timing: "8:00AM to 8:00PM",
+	},
+	{
+		id: 2,
+		name: "Batra Hospital",
+		address:
+			"1, Mehrauli Badarpur Road, Tughlakabad Institutional Area Tughlakabad, New Delhi - 110062",
+		contactNo: 8759856466,
+		availabilityStatus: "full",
+		timing: "8:00AM to 8:00PM",
+	},
+	{
+		id: 3,
+		name: "Orchid Hospital",
+		address:
+			"C-3/91-92, Near Fly Over, Dabri More Janakpuri, New Delhi - 110058",
+		contactNo: 8759856466,
+		availabilityStatus: "free",
+		timing: "8:00AM to 8:00PM",
+	},
+	{
+		id: 4,
+		name: "Medanta Hospital",
+		address: "E – 18 Defence Colony , New Delhi - 110024",
+		contactNo: 8759856466,
+		availabilityStatus: "moderate",
+		timing: "8:00AM to 8:00PM",
+	},
+	{
+		id: 5,
+		name: "Vardaan Hospital",
+		address:
+			"B-100, Near Malviya Nagar Market Malviya Nagar, New Delhi - 110017",
+		contactNo: 8759856466,
+		availabilityStatus: "full",
+		timing: "8:00AM to 8:00PM",
+	},
 ];
 
 export default HospitalList;
